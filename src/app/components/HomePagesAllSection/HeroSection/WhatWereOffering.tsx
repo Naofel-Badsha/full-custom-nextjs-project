d989916500ccf10bad45c424cea6dcb7fc867c24
Naofel-Badsha <naofelbadsha@gmail.com>
2026-02-25 13:16:32 +0600

    added new section

---
+++ src/app/components/HomePagesAllSection/HeroSection/WhatWereOffering.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const WhatWereOffering = () => {
+  return (
+    <section className='py-[100px] bg-[#EAF3EA]'>
+        <div className="max-w-7xl mx-auto">
+            
+        </div>
+    </section>
+  )
+}
+
+export default WhatWereOffering
