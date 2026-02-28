621da2f3d39da5e9b45fe12909fbf826fcb2c49e
Naofel-Badsha <naofelbadsha@gmail.com>
2026-02-28 19:25:43 +0600

    added team member oboject and type declieration

--- src/app/components/HomePagesAllSection/TeamMembers.tsx
+++ src/app/components/HomePagesAllSection/TeamMembers.tsx
@@ -1,5 +1,87 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaFacebook, FaInstagram, FaShare, FaTiktok, FaTwitter } from "react-icons/fa";
+
+interface SocialLink {
+  id: number;
+  icon: React.ReactNode;
+  link: string;
+}
+
+interface TeamMember {
+  id: string,
+  name: string,
+  podobi: string,
+  image: string
+  socials: SocialLink[]
+}
+
+
+const teams: TeamMember[] = [
+  {
+    id: '1',
+    name: 'Hendrik Morella',
+    podobi: 'Insurance Expert',
+    image: '/assets/team-1.webp',
+    socials: [
+      { id: 1, icon: <FaFacebook />, link: 'https://facebook.com' },
+      { id: 2, icon: <FaTwitter />, link: 'https://twitter.com' },
+      { id: 3, icon: <FaInstagram />, link: 'https://instagram.com' },
+      { id: 4, icon: <FaTiktok />, link: 'https://tiktok.com/' },
+    ]
+  },
+  {
+    id: '2',
+    name: 'Bryan Walker',
+    podobi: 'Insurance Expert',
+    image: '/assets/team-2.webp',
+    socials: [
+      { id: 1, icon: <FaFacebook />, link: 'https://facebook.com' },
+      { id: 2, icon: <FaTwitter />, link: 'https://twitter.com' },
+      { id: 3, icon: <FaInstagram />, link: 'https://instagram.com' },
+      { id: 4, icon: <FaTiktok />, link: 'https://tiktok.com/' },
+    ]
+  },
+  {
+    id: '3',
+    name: 'Carryn Zenith',
+    podobi: 'Insurance Expert',
+    image: '/assets/team-3.webp',
+    socials: [
+      { id: 1, icon: <FaFacebook />, link: 'https://facebook.com' },
+      { id: 2, icon: <FaTwitter />, link: 'https://twitter.com' },
+      { id: 3, icon: <FaInstagram />, link: 'https://instagram.com' },
+      { id: 4, icon: <FaTiktok />, link: 'https://tiktok.com/' },
+    ]
+  },
+  {
+    id: '4',
+    name: 'Paul Moralez',
+    podobi: 'Insurance Expert',
+    image: '/assets/team-4.webp',
+    socials: [
+      { id: 1, icon: <FaFacebook />, link: 'https://facebook.com' },
+      { id: 2, icon: <FaTwitter />, link: 'https://twitter.com' },
+      { id: 3, icon: <FaInstagram />, link: 'https://instagram.com' },
+      { id: 4, icon: <FaTiktok />, link: 'https://tiktok.com/' },
+    ]
+  },
+  {
+    id: '5',
+    name: 'james Smaits',
+    podobi: 'Insurance Expert',
+    image: '/assets/team-5.webp',
+    socials: [
+      { id: 1, icon: <FaFacebook />, link: 'https://facebook.com' },
+      { id: 2, icon: <FaTwitter />, link: 'https://twitter.com' },
+      { id: 3, icon: <FaInstagram />, link: 'https://instagram.com' },
+      { id: 4, icon: <FaTiktok />, link: 'https://tiktok.com/' },
+    ]
+  },
+]
+
+
 
 const TeamMembers = () => {
   return (
@@ -18,7 +100,8 @@ const TeamMembers = () => {
           </p>
         </div>
         {/*---------Team------Member---------*/}
-        <div className="">
+        <div className="mt-12">
+          {/*----------Card--------1----------*/}
           <div className="w-[350px] rounded-2xl overflow-hidden shadow-lg bg-white">
             {/* Image Section */}
             <div className="relative">
@@ -44,51 +127,38 @@ const TeamMembers = () => {
                 <div className="relative group flex items-center">
                   {/* Social Icons */}
                   <div
-                    className="
-              absolute -top-12 right-0
-              flex gap-2
-              opacity-0 translate-x-6
-              group-hover:opacity-100
-              group-hover:translate-x-0
-              transition-all duration-300
-            "
+                    className=" absolute -top-48 right-0 space-y-2 gap-2 opacity-0 translate-x-6 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"
                   >
-                    <a
+                    <Link
                       href="#"
-                      className="w-8 h-8 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
+                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
                     >
-                      F
-                    </a>
-                    <a
+                      <FaFacebook />
+                    </Link>
+                    <Link
                       href="#"
-                      className="w-8 h-8 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
+                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
                     >
-                      T
-                    </a>
-                    <a
+                      <FaTwitter />
+                    </Link>
+                    <Link
                       href="#"
-                      className="w-8 h-8 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
+                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
                     >
-                      I
-                    </a>
+                      <FaInstagram />
+                    </Link>
+                    <Link
+                      href="#"
+                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
+                    >
+                      <FaTiktok />
+                    </Link>
                   </div>
 
                   {/* Share Button */}
                   <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center cursor-pointer relative z-10">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={2}
-                      stroke="currentColor"
-                      className="w-5 h-5 text-white"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M8 12h8m0 0l-3-3m3 3l-3 3"
-                      />
-                    </svg>
+
+                    <FaShare className="w-5 h-5 text-white" />
                   </div>
                 </div>
               </div>
