4f3a0df61a73e14d84c800b9569be38a2c02cf77
Naofel-Badsha <naofelbadsha@gmail.com>
2026-02-28 20:25:20 +0600

    added slider team member

--- src/app/components/HomePagesAllSection/TeamMembers.tsx
+++ src/app/components/HomePagesAllSection/TeamMembers.tsx
@@ -1,3 +1,10 @@
+'use client'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, } from 'swiper/modules';
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -100,71 +107,91 @@ const TeamMembers = () => {
           </p>
         </div>
         {/*---------Team------Member---------*/}
-        <div className="mt-12">
-          {/*----------Card--------1----------*/}
-          <div className="w-[350px] rounded-2xl overflow-hidden shadow-lg bg-white">
-            {/* Image Section */}
-            <div className="relative">
-              <Image
-                src="/assets/professiona-2.webp"
-                alt="Team Member"
-                width={500}
-                height={500}
-                className="w-full h-[420px] object-cover shadow p-3"
-              ></Image>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={20}
+          centeredSlides={true}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          // pagination={{
+          //   clickable: true,
+          // }}
 
-              {/* Bottom Info Card */}
-              <div className="absolute bottom-6 left-6 right-6 bg-green-800 rounded-xl px-6 py-4 flex items-center justify-between">
-                {/* Name + Role */}
-                <div>
-                  <h3 className="text-white text-xl font-semibold">
-                    Bryan Walker
-                  </h3>
-                  <p className="text-green-200 text-sm">Insurance Expert</p>
-                </div>
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              centeredSlides: false,
+            },
+            1024: {
+              slidesPerView: 3,
+              centeredSlides: false,
+            },
+            1280: {
+              slidesPerView: 4,
+              centeredSlides: false,
+            },
+          }}
+          modules={[Autoplay, Pagination]}
+          className="mySwiper"
+        >
+          <div>
+            {
+              teams.map((team) => (
+                <SwiperSlide key={team.id}>
+                  <div className="mt-6 py-5">
+                    <div className=" rounded-2xl overflow-hidden shadow-lg bg-white">
+                      <div className="relative">
+                        <Image
+                          src={team.image}
+                          alt="Team Member"
+                          width={500}
+                          height={500}
+                          className="w-full h-[420px] object-cover shadow p-3"
+                        ></Image>
+                        <div className="absolute bottom-6 left-6 right-6 bg-green-800 rounded-xl px-6 py-4 flex items-center justify-between">
+                          {/*---------Name+Role-------------*/}
+                          <div>
+                            <h3 className="text-white text-xl font-semibold">
+                              {team.name}
+                            </h3>
+                            <hr className='text-white h-[1px] mt-2 mb-2'/>
+                            <p className="text-green-200 text-sm">{team.podobi}</p>
+                          </div>
 
-                {/* Share + Social */}
-                <div className="relative group flex items-center">
-                  {/* Social Icons */}
-                  <div
-                    className=" absolute -top-48 right-0 space-y-2 gap-2 opacity-0 translate-x-6 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"
-                  >
-                    <Link
-                      href="#"
-                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
-                    >
-                      <FaFacebook />
-                    </Link>
-                    <Link
-                      href="#"
-                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
-                    >
-                      <FaTwitter />
-                    </Link>
-                    <Link
-                      href="#"
-                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
-                    >
-                      <FaInstagram />
-                    </Link>
-                    <Link
-                      href="#"
-                      className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
-                    >
-                      <FaTiktok />
-                    </Link>
+                          {/*--------Share-------Social-----------*/}
+                          <div className="relative group flex items-center">
+                            <div
+                              className=" absolute -top-48 right-0 space-y-2 gap-2 opacity-0 translate-x-6 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"
+                            >
+                              {team.socials.map((social) => (
+                              <Link
+                              key={social.id}
+                                href={social.link}
+                                target="_blank"
+                                className="w-8 h-8 hover:bg-[#00A63E] hover:text-white hover:scale-110 duration-500 bg-white rounded-full flex items-center justify-center text-green-800 text-sm font-bold shadow"
+                              >
+                                {social.icon}
+                              </Link>
+                              ))}
+                            </div>
+
+                            {/*---------Share-------Button----------*/}
+                            <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center cursor-pointer relative z-10">
+                              <FaShare className="w-5 h-5 text-white" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-
-                  {/* Share Button */}
-                  <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center cursor-pointer relative z-10">
-
-                    <FaShare className="w-5 h-5 text-white" />
-                  </div>
-                </div>
-              </div>
-            </div>
+                </SwiperSlide>
+              ))
+            }
           </div>
-        </div>
+        </Swiper>
       </div>
     </section>
   );
