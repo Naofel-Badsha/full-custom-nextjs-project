b0201813d7d83fe92ee8250d1081ae1efede510c
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-04 11:41:58 +0600

    added aboutus section in the about page

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,10 +1,12 @@
 import AboutBanner from '@/app/components/AboutPageAllSection/AboutBanner'
+import AboutUs from '@/app/components/AboutPageAllSection/AboutUs'
 import React from 'react'
 
 const AboutPage = () => {
   return (
     <div className=''>
       <AboutBanner />
+      <AboutUs />
     </div>
   )
 }
