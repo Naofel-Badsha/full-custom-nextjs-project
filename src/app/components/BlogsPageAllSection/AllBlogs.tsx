b35fda9a18e3e6131bac7531ef4a965213c5e4b7
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-16 04:56:29 +0600

    added all blog page modification

--- src/app/components/BlogsPageAllSection/AllBlogs.tsx
+++ src/app/components/BlogsPageAllSection/AllBlogs.tsx
@@ -1,24 +1,33 @@
-import { Blog } from '@/app/types/blog';
-import React from 'react'
+import { Blog } from "@/app/types/blog";
+import React from "react";
 
 async function getBlogs(): Promise<Blog[]> {
-   const res = await fetch('https://dev.to/api/articles', {
-    next: {revalidate: 60}
-   }) 
-   return res.json()
-   
+  const res = await fetch("https://dev.to/api/articles", {
+    next: { revalidate: 60 },
+  });
+  return res.json();
 }
-
 
 const AllBlogs = async () => {
-   const blogs: Blog[] = await getBlogs();
-
+  const blogs: Blog[] = await getBlogs();
 
   return (
-    <section className=''>
-        <h1>All Blogs{blogs.length}</h1>
+    <section className="py-[100px] bg-[#EAF3EA]">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="bg-white p-5 rounded-xl shadow border-[1px] border-gray-200"
+            >
+              <h2 className="">{blog.title}</h2>
+              <p className="">{blog.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AllBlogs
+export default AllBlogs;
