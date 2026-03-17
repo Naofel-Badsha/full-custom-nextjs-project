283ff09f476b553a23c2206fac5812191483dd06
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-18 04:04:02 +0600

    added no data by single page

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-};
-
-module.exports = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+        pathname: "/**",
       },
     ],
   },
-}
+};
+
+
 
 export default nextConfig;
--- src/app/components/BlogsPageAllSection/AllBlogs.tsx
+++ src/app/components/BlogsPageAllSection/AllBlogs.tsx
@@ -3,28 +3,27 @@
 import { Blog } from "@/app/types/blog";
 import Image from "next/image";
 import Link from "next/link";
-import { FaCalendar} from "react-icons/fa";
+import { FaCalendar } from "react-icons/fa";
 import { useEffect, useState } from "react";
 
-const AllBlogs =  () => {
+const AllBlogs = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [search, setSearch] = useState("");
   console.log(search)
+  //---------fetch blogs-------
+  useEffect(() => {
+    const getBlogs = async () => {
+      const res = await fetch("https://dev.to/api/articles");
+      const data = await res.json()
+      setBlogs(data);
+    };
+    getBlogs();
+  }, []);
 
+  //--------filter blogs-------
   const blogFilter = blogs.filter((blog) =>
     blog.title?.toLowerCase().includes(search.toLowerCase())
   )
-
-  //---------fetch blogs-------
-  useEffect(() => {
-      const getBlogs = async() => {
-        const res = await fetch("https://dev.to/api/articles");
-        const data = await res.json()
-        setBlogs(data);
-      };
-      getBlogs();
-  }, []);
-  
 
   return (
     <section className="py-[100px] bg-[#EAF3EA]">
@@ -39,34 +38,22 @@ const AllBlogs =  () => {
         </div>
 
         <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
-          {blogs.map((blog) => (
+          {blogFilter.map((blog) => (
             <div
               key={blog.id}
               className="bg-white p-5 rounded-xl shadow border-[1px] border-gray-200 space-y-3"
             >
               {/*----Blog----Cover---Images----*/}
-              <div className="">
-                {blog.cover_image ? (
-                  <Image
-                    src={blog.cover_image}
-                    alt={blog.title}
-                    width={500}
-                    height={500}
-                    className="rounded-xl w-full h-[200px] object-cover"
-                  />
-                ) : (
-                  <Image
-                    src="/assets/blog-1.webp"
-                    alt="placeholder"
-                    width={500}
-                    height={500}
-                    className="rounded-xl w-full h-[200px] object-cover"
-                  />
-                )}
-              </div>
+              <Image
+                src={blog.cover_image || "/assets/blog-1.webp"}
+                alt={blog.title}
+                width={500}
+                height={500}
+                className="rounded-xl w-full h-[200px] object-cover"
+              />
 
               {/*--------Details------*/}
-              <h2 className="text-[20px]">{blog.title.slice(1, 40)}..</h2>
+              <h2 className="text-[20px]">{blog.title.slice(0, 40)}..</h2>
               <p className="text-[15px]">{blog.description.slice(1, 120)}...</p>
               <div className="flex items-center justify-between gap-2 mt-5">
                 <div className="flex items-center gap-2">
