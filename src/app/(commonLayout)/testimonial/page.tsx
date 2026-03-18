ab2282ad9cfd10683ac88a42ff168c14847e78dc
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-19 02:08:15 +0600

    added all testimonial

--- src/app/(commonLayout)/testimonial/page.tsx
+++ src/app/(commonLayout)/testimonial/page.tsx
@@ -1,3 +1,4 @@
+import AllTestimonials from '@/app/components/TestimonialAllSection/AllTestimonials'
 import TestimainalBanner from '@/app/components/TestimonialAllSection/TestimainalBanner'
 import Link from 'next/link'
 import React from 'react'
@@ -6,6 +7,7 @@ const TestimonialPage = () => {
   return (
     <div>
       <TestimainalBanner />
+      <AllTestimonials />
     </div>
   )
 }
