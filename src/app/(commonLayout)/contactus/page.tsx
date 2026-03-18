d3d279db512a922e385ec6a2babaf219d6758827
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-19 02:55:16 +0600

    added info card

--- src/app/(commonLayout)/contactus/page.tsx
+++ src/app/(commonLayout)/contactus/page.tsx
@@ -1,10 +1,40 @@
 import Link from 'next/link'
-import React from 'react'
+
+import { ReactNode } from "react";
+import { FaMapPin, FaPhone, FaVoicemail } from 'react-icons/fa';
+// Type Define
+type ContactItemType = {
+  id: number;
+  title: string;
+  value: string;
+  icon: ReactNode;
+};
+
+const contactData: ContactItemType[] = [
+  {
+    id: 1,
+    title: "Email",
+    value: "support@domain.com",
+    icon: <FaVoicemail />,
+  },
+  {
+    id: 2,
+    title: "Phone",
+    value: "+1 (555) 000-0000",
+    icon: <FaPhone/>,
+  },
+  {
+    id: 3,
+    title: "Location",
+    value: "123 Street, New York, USA",
+    icon: <FaMapPin/>,
+  },
+];
 
 const ContactUsPage = () => {
   return (
     <div>
-       <section className="">
+      <section className="">
         {/*------------Banner-------------*/}
         <div
           className="relative w-full bg-[#006b33] overflow-hidden"
@@ -46,6 +76,28 @@ const ContactUsPage = () => {
       </section>
 
       {/* -------Box-------Card---*/}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto py-[100px]">
+        {contactData.map((item) => (
+          <div
+            key={item.id}
+            className="flex items-center gap-4 p-6 bg-white/10 backdrop-blur-md border border-gray-200 rounded-xl shadow"
+          >
+            <div className="text-gray-800 bg-green-500 rounded-full">
+              <div className="text-[20px] p-4 text-white">{item.icon}</div>
+            </div>
+
+            <div className="flex flex-col">
+              <span className="text-gray-800 text-[18px] font-semibold">
+                {item.title}:
+              </span>
+              <span className="text-gray-400 text-lg font-light">
+                {item.value}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+
       
     </div>
   )
