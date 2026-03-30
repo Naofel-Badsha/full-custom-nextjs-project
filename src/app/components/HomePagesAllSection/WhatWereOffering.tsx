c05dfa6344e81a52638b732a02204f88b3f87cdb
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-30 22:08:18 +0600

    added What we're offering responsive done

--- src/app/components/HomePagesAllSection/WhatWereOffering.tsx
+++ src/app/components/HomePagesAllSection/WhatWereOffering.tsx
@@ -59,16 +59,16 @@ const offerings: OfferingType[] = [
 const WhatWereOffering = () => {
   return (
     <section className="py-[100px] bg-[#EAF3EA]">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto px-6">
         {/*------------Content-----------*/}
         <div className="space-y-5">
-          <div className="w-fit py-1.5 px-4 shadow-md bg-white rounded-full text-[#01A04C] border-[1px] border-gray-200 font-semibold m-auto">
+          <div className="w-fit py-1.5 px-4 shadow-md bg-white rounded-full text-[#01A04C] border-[1px] border-gray-200 font-medium m-auto">
             What we're offering
           </div>
-          <h2 className="text-[#1d1d1d] font-semibold text-[42px] capitalize leading-13 text-center mt-5">
+          <h2 className="text-[#1d1d1d] font-semibold text-[30px] lg:text-[42px] capitalize leading-13 text-center mt-5">
             Highlighting services or <br /> insurance plans.
           </h2>
-          <p className="text-[18px] font-light text-[#6d6d6d] text-center w-[600px] m-auto">
+          <p className="text-[16px] lg:text-[18px] font-light text-[#6d6d6d] text-center w-[600px] m-auto">
             Sagittis mi eros purus commodo himenaeos ac. Primis ligula ligula
             ultricies lacinia interdum etiam proin. Blandit fames finibus
             consequat luctus.
