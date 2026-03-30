a4e68a48eb316caf00220a6b246e0666b3e4c156
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-30 22:37:04 +0600

    added Case Study responsive

--- src/app/components/HomePagesAllSection/CaseStudy.tsx
+++ src/app/components/HomePagesAllSection/CaseStudy.tsx
@@ -67,16 +67,16 @@ const studys: StydysTypes[] = [
 const CaseStudy = () => {
     return (
         <section className="py-[100px] bg-[#EAF3EA]">
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-7xl mx-auto px-6">
                 {/*------------Content-----------*/}
                 <div className="space-y-5">
                     <div className="w-fit py-1.5 px-4 shadow-md bg-white rounded-full text-[#01A04C] border-[1px] border-gray-200 font-medium m-auto">
                         Case Study
                     </div>
-                    <h2 className="text-[#1d1d1d] font-semibold text-[42px] capitalize leading-13 text-center mt-5">
-                        Our Effective Insurence Services <br /> & Solutions.
+                    <h2 className="text-[#1d1d1d] font-semibold text-[30px] lg:text-[42px] capitalize  text-center mt-5">
+                        Our Effective Insurence Services <br className='hidden lg:block' /> & Solutions.
                     </h2>
-                    <p className="text-[18px] font-light text-[#6d6d6d] text-center w-[600px] m-auto">
+                    <p className="text-[16px] lg:text-[18px] font-light text-[#6d6d6d] text-center w-[600px] m-auto">
                         Tortor ligula pulvinar finibus primis eget tristique.
                     </p>
                 </div>
@@ -112,8 +112,8 @@ const CaseStudy = () => {
                                     </div>
 
                                     <div className="mt-4">
-                                        <h2 className="font-medium text-[27px] text-[#1d1d1d]">Business Insurance</h2>
-                                        <p className="font-light text-[18px] text-[#6d6d6d] mt-2">Lacinia habitant auctor aenean et inceptos tincidunt blandit. Est faucibus a faucibus; facilisis finibus turpis…</p>
+                                        <h2 className="font-medium text-[20px] lg:text-[25px] text-[#1d1d1d]">{study.name}</h2>
+                                        <p className="font-light text-[14px] lg:text-[18px] text-[#6d6d6d] mt-2">{study.description.slice(0, 70)}...</p>
                                     </div>
 
                                     <div className="flex items-center ">
