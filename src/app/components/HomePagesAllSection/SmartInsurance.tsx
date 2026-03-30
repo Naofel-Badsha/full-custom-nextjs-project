35d37ba665c8697ac0e1237e586fc097b01dba94
Naofel-Badsha <naofelbadsha@gmail.com>
2026-03-30 22:24:13 +0600

    added Smart Insurance responsive

--- src/app/components/HomePagesAllSection/SmartInsurance.tsx
+++ src/app/components/HomePagesAllSection/SmartInsurance.tsx
@@ -5,12 +5,12 @@ import { BiDownload } from "react-icons/bi";
 const SmartInsurance = () => {
   return (
     <section className="py-[100px] bg-white">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto px-6">
         {/*------------Banner-------------*/}
         <div className="flex items-center justify-center">
           {/*---------Main Container with Diamond Pattern/Gradient---------*/}
           <div
-            className="relative w-full  bg-[#006b33] rounded-3xl overflow-hidden flex flex-col lg:flex-row"
+            className="relative w-full p-6 md:p-10  bg-[#006b33] rounded-3xl overflow-hidden flex flex-col lg:flex-row"
             style={{
               backgroundImage: `radial-gradient(circle at 20% 50%, #008445 0%, #006b33 100%)`,
             }}
@@ -24,26 +24,26 @@ const SmartInsurance = () => {
             ></div>
 
             {/*--------Left Content Area---------*/}
-            <div className="w-full p-8 md:p-16 z-10 text-white">
-              <span className="bg-white text-[#00a352] px-4 py-1.5 rounded-full text-sm font-semibold mb-6 inline-block">
+            <div className="w-full  z-10 text-white">
+              <span className="bg-white text-[#00a352] px-4 py-1.5 rounded-full text-sm font-medium mb-6 inline-block">
                 Smart Insurance
               </span>
 
-              <h1 className="text-[42px] font-bold leading-12 mb-8">
+              <h1 className="text-[30px] lg:text-[42px] font-bold leading-12 mb-8">
                 Apply For Your Life Insurance.
               </h1>
             </div>
 
             {/*----------Middal Card----------*/}
-            <div className="w-full m-4 md:mr-8 md:my-8">
+            <div className="w-full m-4">
               {/* Agent Header */}
               <div className="max-w-md space-y-6 text-[#e6e6e6]">
-                <p className="text-[18px] leading-relaxed">
+                <p className="text-[16px] lg:text-[18px]">
                   Proin ultricies vel urna pulvinar pulvinar. Morbi condimentum
                   dolor vel tellus laoreet, eu auctor erat sodales suspendisse
                   dictum.
                 </p>
-                <p className="text-[18px] leading-relaxed">
+                <p className="text-[16px] lg:text-[18px]">
                   Nostra dignissim magna justo dictum primis; mauris diam.
                   Natoque ex suspendisse nostra fames.
                 </p>
@@ -51,19 +51,19 @@ const SmartInsurance = () => {
                 <ul className="space-y-4 pt-4">
                   <li className="flex items-start gap-3">
                     <CheckBadgeIcon className="w-5 h-5 mt-0.5 text-white" />
-                    <span className="text-[18px]">
+                    <span className="text-[16px] lg:text-[18px]">
                       Lorem cubilia auctor tristique habitasse.
                     </span>
                   </li>
                   <li className="flex items-start gap-3">
                     <CheckBadgeIcon className="w-5 h-5 mt-0.5 text-white" />
-                    <span className="text-[18px]">
+                    <span className="text-[16px] lg:text-[18px]">
                       Tristique etiam inceptos tempus quis a nascetur.
                     </span>
                   </li>
                   <li className="flex items-start gap-3">
                     <CheckBadgeIcon className="w-5 h-5 mt-0.5 text-white" />
-                    <span className="text-[18px]">
+                    <span className="text-[16px] lg:text-[18px]">
                       Blandit class malesuada dapibus vestibulum.
                     </span>
                   </li>
@@ -72,7 +72,7 @@ const SmartInsurance = () => {
             </div>
 
             {/*----------Right Sidebar Card----------*/}
-            <div className="w-full bg-[#222222] p-6 m-4 md:mr-8 md:my-8 rounded-2xl flex flex-col z-20">
+            <div className="w-full bg-[#222222] p-6 md:mt-10 lg:mt-0 rounded-2xl flex flex-col z-20">
               {/* Agent Header */}
               <div className="bg-[#00a352] p-4 rounded-xl flex items-center gap-4 mb-6">
                 <div className="w-12 h-12 rounded-full border-2 border-white overflow-hidden bg-gray-200">
