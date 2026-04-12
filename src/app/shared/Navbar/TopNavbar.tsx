43cdc4bbefd59131c04d81d9503e1729b371e900
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-12 23:41:52 +0600

    added top navbar responsive

--- src/app/shared/Navbar/TopNavbar.tsx
+++ src/app/shared/Navbar/TopNavbar.tsx
@@ -14,36 +14,36 @@ const TopNavbar = () => {
   return (
     <div className="bg-[#005224] ">
       <div className="flex items-center justify-between mx-auto max-w-7xl px-6 py-5 lg:px-8">
-        <div className="flex gap-4">
+        <div className="flex flex-wrap gap-1.5 lg:gap-4">
           {/*-------1-------*/}
-          <div className="flex gap-1.5 cursor-pointer group ">
-            <FaMapMarkerAlt className="text-xl text-[#019A49] group-hover:text-white" />
-            <p className="text-white group-hover:text-white">KLLG st, No 99,Pku City</p>
+          <div className="flex gap-1.5 cursor-pointer group">
+            <FaMapMarkerAlt className="text-[16px] lg:text-[20px] text-[#019A49] group-hover:text-white" />
+            <p className="text-white group-hover:text-white text-[13px] lg:text-[15px]">KLLG st, No 99,Pku City</p>
           </div>
           {/*-------1-------*/}
           <div className="flex gap-1.5 cursor-pointer group">
-            <FaPhoneVolume className="text-xl text-[#019A49] group-hover:text-white" />
-            <p className="text-white group-hover:text-white">+(123) 4567 7890</p>
+            <FaPhoneVolume className="text-[16px] lg:text-[20px] text-[#019A49] group-hover:text-white" />
+            <p className="text-white group-hover:text-white text-[13px] lg:text-[15px]">+(123) 4567 7890</p>
           </div>
           {/*-------1-------*/}
           <div className="flex gap-1.5 cursor-pointer group">
-            <FaEnvelopeOpenText className="text-xl text-[#019A49] group-hover:text-white" />
-            <p className="text-white group-hover:text-white">@emailexample.com</p>
+            <FaEnvelopeOpenText className="text-[16px] lg:text-[20px] text-[#019A49] group-hover:text-white" />
+            <p className="text-white group-hover:text-white text-[13px] lg:text-[15px]">info@gmail.com</p>
           </div>
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex flex-wrap gap-1.5 lg:gap-4">
           <Link href="">
-            <FaFacebook className="text-xl text-white hover:rotate-360 duration-500" />
+            <FaFacebook className="text-[16px] lg:text-[20px] text-white hover:rotate-360 duration-500" />
           </Link>
           <Link href="">
-            <FaTwitter className="text-xl text-white hover:rotate-360 duration-500" />
+            <FaTwitter className="text-[16px] lg:text-[20px] text-white hover:rotate-360 duration-500" />
           </Link>
           <Link href="">
-            <FaInstagram className="text-xl text-white hover:rotate-360 duration-500" />
+            <FaInstagram className="text-[16px] lg:text-[20px] text-white hover:rotate-360 duration-500" />
           </Link>
           <Link href="">
-            <FaLinkedin className="text-xl text-white hover:rotate-360 duration-500" />
+            <FaLinkedin className="text-[16px] lg:text-[20px] text-white hover:rotate-360 duration-500" />
           </Link>
         </div>
       </div>
